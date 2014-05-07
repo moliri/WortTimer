d68f600b335f86a2330f3178f911423ecda74613
Daniel Zuo <danielpengzuo@gmail.com>
2014-05-07 17:57:43 -0500

    a preliminary functional time input page with timer

--- js/WortTimer.js
+++ js/WortTimer.js
@@ -1,27 +1,32 @@
 "use strict";
 
+var firstPhase;
 
 /* start page events and functions */
 /* starting script for intro page */
-$(document).on('pageinit', '#intropage', function(){
-    
+$(document).on('pageinit', '#timeInput', function(){
+	$('#submitTime').click(function () {
+		//alert($('#firstPhase').val());
+		$('#CountDownTimer').attr('data-timer', $('#firstPhase').val());
+		$.mobile.changePage('#timer');
+	});
 });
 
 /* Timer page events and functions */
 /* starting script for timer page */
 $(document).on('pageinit','#timer',function() {
-    
-});
-
-
-/* about page events and functions */
-/* starting script for about page */
-$(document).on('pageinit','#about',function(){
-    $('#favList2').click(getFavorites);
-});
-
-/* follow us page events and functions */
-/* starting script for follow page */
-$(document).on('pageinit','#contact',function(){
-    $('#favList3').click(getFavorites);
+            $("#CountDownTimer").TimeCircles({
+            	"time" : { "Days": { "show": false }, "Hours": { "show": false }},
+            	"count_past_zero": false
+            });
+            //$("#CountDownTimer").TimeCircles().stop();
+            
+            // Start and stop are methods applied on the public TimeCircles instance
+            $(".startTimer").click(function() {
+                $("#CountDownTimer").TimeCircles().start();
+            });
+           
+            $(".stopTimer").click(function() {
+                $("#CountDownTimer").TimeCircles().stop();
+            });       
 });
