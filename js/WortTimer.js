9669f999d13b4a15b63b9aaec4e68ec3cdd2e698
haodongwang <haodongwang2013@u.northwestern.edu>
2014-05-09 15:35:34 -0500

    small second circle

--- js/WortTimer.js
+++ js/WortTimer.js
@@ -8,6 +8,7 @@ $(document).on('pageinit', '#timeInput', function(){
 	$('#submitTime').click(function () {
 		//alert($('#firstPhase').val());
 		$('#CountDownTimer').attr('data-timer', $('#firstPhase').val());
+        $('#CountDownTimer2').attr('data-timer', $('#firstPhase').val());
 		$.mobile.changePage('#timer');
 	});
 });
@@ -16,18 +17,24 @@ $(document).on('pageinit', '#timeInput', function(){
 /* starting script for timer page */
 $(document).on('pageinit','#timer',function() {
             $("#CountDownTimer").TimeCircles({
-            	"time" : { "Days": { "show": false }, "Hours": { "show": false }},
+            	"time" : { "Days": { "show": false }, "Hours": { "show": false },"Seconds":{"show":false}},
             	"count_past_zero": false
+            });
+            $("#CountDownTimer2").TimeCircles({
+                "time" : { "Days": { "show": false }, "Hours": { "show": false },"Minutes":{"show":false}},
+                "count_past_zero": false
             });
             //$("#CountDownTimer").TimeCircles().stop();
             
             // Start and stop are methods applied on the public TimeCircles instance
             $(".startTimer").click(function() {
                 $("#CountDownTimer").TimeCircles().start();
+                $("#CountDownTimer2").TimeCircles().start();
             });
            
             $(".stopTimer").click(function() {
                 $("#CountDownTimer").TimeCircles().stop();
+                $("#CountDownTimer2").TimeCircles().stop();
             });       
 
             $("#CountDownTimer").TimeCircles().addListener(timeElapsed, "visible");
